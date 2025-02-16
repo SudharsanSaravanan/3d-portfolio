97842a8c0e1354663451794a8240cf4375248059
SUDHARSAN SARAVANAN <sudharsansaravanan2623@gmail.com>
2025-02-16 19:26:38 +0530

    assets updated commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,29 +1,8 @@
-import { meta, shopify, starbucks, tesla } from "../assets/images";
+import { personal } from "../assets/images";
 import {
-    car,
     contact,
-    css,
-    estate,
-    express,
-    git,
     github,
-    html,
-    javascript,
-    linkedin,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    pricewise,
-    react,
-    redux,
-    sass,
-    snapgram,
-    summiz,
-    tailwindcss,
-    threads,
-    typescript
+    linkedin
 } from "../assets/icons";
 
 export const skills = [
@@ -146,58 +125,20 @@ export const skills = [
 
 export const experiences = [
     {
-        title: "React.js Developer",
-        company_name: "Starbucks",
-        icon: starbucks,
+        title: "React Web Application Developer",
+        company_name: "Personal",
+        icon: personal,
         iconBg: "#accbe1",
-        date: "March 2020 - April 2021",
+        date: "Dec 2024 - Jan 2025",
         points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
+            "Led the development of multiple React applications and mentored junior developers.",
+            "Built 'Leaf Link', a real-time messaging app using React.js and Firestore, implementing responsive UI with Bootstrap 5 and Material UI.",
+            "Developed 'Moltov', a movie trailer streaming app that fetches real-time data from TMDB API, showcasing expertise in API integration and state management.",
+            "Focused on creating seamless user experiences by efficiently handling application state and optimizing performance.",
         ],
-    },
-    {
-        title: "React Native Developer",
-        company_name: "Tesla",
-        icon: tesla,
-        iconBg: "#fbc3bc",
-        date: "Jan 2021 - Feb 2022",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Web Developer",
-        company_name: "Shopify",
-        icon: shopify,
-        iconBg: "#b7e4c7",
-        date: "Jan 2022 - Jan 2023",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
-    {
-        title: "Full stack Developer",
-        company_name: "Meta",
-        icon: meta,
-        iconBg: "#a2d2ff",
-        date: "Jan 2023 - Present",
-        points: [
-            "Developing and maintaining web applications using React.js and other related technologies.",
-            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-            "Implementing responsive design and ensuring cross-browser compatibility.",
-            "Participating in code reviews and providing constructive feedback to other developers.",
-        ],
-    },
+    }
 ];
+
 
 export const socialLinks = [
     {
@@ -219,45 +160,27 @@ export const socialLinks = [
 
 export const projects = [
     {
-        iconUrl: pricewise,
+        iconUrl: "#",  // Replace with actual image URL or asset
         theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-        link: 'https://github.com/adrianhajdin/pricewise',
+        name: 'Moltov - Movie Trailer Streaming App',
+        description: 'A React.js-based movie streaming app that fetches real-time movie data from TMDB API. The app features an intuitive UI, efficient state management, and smooth API integrations.',
+        technologies: ["React.js", "Node.js", "Axios", "Firebase"],
+        link: 'https://github.com/SudharsanSaravanan/MOLTOV-StreamingApp',
     },
     {
-        iconUrl: threads,
+        iconUrl: "#",  // Replace with actual image URL or asset
         theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
+        name: 'Leaf-Link - Real-Time Messaging App',
+        description: 'Developed a messaging application using React.js and Firestore, with responsive UI implemented using Bootstrap 5 and Material UI. Ensured smooth real-time data synchronization for instant communication.',
+        technologies: ["React.js", "Firestore", "Material UI"],
+        link: 'https://github.com/SudharsanSaravanan/Leaf-Link-MessagingApp',
     },
     {
-        iconUrl: car,
+        iconUrl: "",  // Replace with actual image URL or asset
         theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-    },
-    {
-        iconUrl: snapgram,
-        theme: 'btn-back-pink',
-        name: 'Full Stack Instagram Clone',
-        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
-    },
-    {
-        iconUrl: summiz,
-        theme: 'btn-back-yellow',
-        name: 'AI Summarizer Application',
-        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+        name: 'Hospital Management System',
+        description: 'A DSA-based project utilizing Min-Heap Priority Queue, Graph (Adjacency List), and Binary Search Tree (BST) to prioritize patient care. Implemented Dijkstra’s algorithm for efficient room allocation, streamlining hospital operations.',
+        technologies: ["Python", "Heap", "Graph Algorithms", "Dijkstra’s Algorithm"],
+        link: 'https://github.com/SudharsanSaravanan/Hospital_Mangement_System',
     }
 ];
